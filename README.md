12eb2da5d461deb159d424f53649c167482c0289
ehdiH <hmehdih@HmehdiH.lan>
2023-11-16 14:13:39 +0100

    scribble source

---
+++ README.md
@@ -0,0 +1,2 @@
+## The project is prepared in group in Object Oriented Application course
+**this repository will be updated soon ...** 
---
+++ src/server/SerializableList.java
@@ -0,0 +1,29 @@
+package server;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+* Serializable data structure to storing data
+* @author Mehdi Haidari
+*/
+public class SerializableList<T> implements Serializable {
+    private List<T> list;
+
+    public SerializableList() {
+        list = Collections.synchronizedList(new ArrayList<>());
+    }
+   
+    public List<T> getList() {
+        return list;
+    }
+
+    public void addMessage(T t){
+        list.add(t);
+    }
+
+
+}
+
